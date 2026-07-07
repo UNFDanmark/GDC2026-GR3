8e5bef62f80b596c03242dbd9cce01f77a942978
GDC2026 Programming <gdc2026-programming@unf.dk>
2026-07-07 21:56:07 +0200

    Update AmbientSounds.cs

--- MainUnityProject/Assets/Code/AmbientSounds.cs
+++ MainUnityProject/Assets/Code/AmbientSounds.cs
@@ -7,13 +7,6 @@ public class AmbientSounds : MonoBehaviour
 {
     [SerializeField] SoundContainer[] sounds;
 
-    void Start()
-    {
-        foreach (var sound in sounds)
-        {
-            AudioSource.PlayClipAtPoint(sound.AudioClip, new Vector3(7.46000004f,3.02999997f,5.53000021f));
-        }
-    }
 }
 [Serializable]
 public class SoundContainer
