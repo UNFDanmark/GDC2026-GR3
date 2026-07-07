7a55f3ad0efe1dbbab438dfe3e3ac9fae99ca0ba
GDC2026 Programming <gdc2026-programming@unf.dk>
2026-07-07 11:06:35 +0200

    Try to get UI working :(

--- MainUnityProject/Assets/Code/PlayerInteract.cs
+++ MainUnityProject/Assets/Code/PlayerInteract.cs
@@ -4,19 +4,30 @@ public class PlayerInteract : MonoBehaviour
 {
     [SerializeField] Transform cameraOrigin;
     [SerializeField] float interactRange = 3;
+    [SerializeField] GameObject interactUI;
+    [SerializeField] Collider rayCastChecker;
+    
     public void Interact()
     {
-        Physics.Raycast(cameraOrigin.position, cameraOrigin.forward, out RaycastHit interactTarget, interactRange);
+       bool rayHit = Physics.Raycast(cameraOrigin.position, cameraOrigin.forward, out RaycastHit interactTarget, interactRange);
+
+        if (rayHit)
+        {
+            interactUI.SetActive(true);
+        }
+        
         Debug.DrawRay(cameraOrigin.position, cameraOrigin.forward, Color.red, 5);
         try
         {
 
             interactTarget.transform.GetComponent<Iinteractable>().Interact();
+            interactUI.SetActive(false);
 
         }
         catch
         {
             
         }
+  
     }
 }
--- MainUnityProject/Assets/Code/TestObject.cs
+++ MainUnityProject/Assets/Code/TestObject.cs
@@ -2,9 +2,17 @@ using UnityEngine;
 
 public class TestObject : MonoBehaviour, Iinteractable
 {
+    [SerializeField] GameObject interactUI;
+    
     public void Interact()
     {
+        gameObject.GetComponent<Collider>().enabled = false;
+        
+        
+        interactUI.SetActive(false);
+        print("UI : " + interactUI.activeSelf);
         Debug.Log("pickup yay");
         Destroy(gameObject);
+        
     }
 }
